69cba5a86b3c553f696eb40024777e83e8e220cc
andreirazv <malaiandrei98@gmail.com>
2020-04-10 02:41:18 +0300

    change build

--- README.md
+++
@@ -1,9 +0,0 @@
-### `node server`
-
-To start websocket server for game.
-
-### `npm start`
-
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import GameView from "./game-view/GameView";
-
-ReactDOM.render(<GameView />, document.getElementById("root"));
-serviceWorker.unregister();
