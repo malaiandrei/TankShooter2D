3a2bcceb7deb15d818408b633354bf577350a634
andreirazv <malaiandrei98@gmail.com>
2020-04-10 02:33:57 +0300

    game

---
+++ build/precache-manifest.e6b71e511358898a6e0debfdb0f8ebb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74463dcbbc63269dabc311563fffb345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5528f0ef36777034f7d3",
+    "url": "/static/css/main.9dfdfd61.chunk.css"
+  },
+  {
+    "revision": "c294e3f4c3da28b7f03b",
+    "url": "/static/js/2.2f70ce60.chunk.js"
+  },
+  {
+    "revision": "a0bbf00212c6f4989ff8397decc5cf1d",
+    "url": "/static/js/2.2f70ce60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5528f0ef36777034f7d3",
+    "url": "/static/js/main.e4e2fed5.chunk.js"
+  },
+  {
+    "revision": "d5e4014b21f2ff4da3a4",
+    "url": "/static/js/runtime-main.2f928ac5.js"
+  }
+]);
